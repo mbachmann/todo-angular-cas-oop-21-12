2fc46a3412a609c14b552ec38b2328b866070471
bacn <mbachmann@trdi.io>
2021-12-17 19:05:03 +0100

    initial step 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from "@angular/common/http";
+import {ApiModule, BASE_PATH} from "./openapi-gen";
+import {environment} from "../environments/environment";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule
   ],
-  providers: [],
+  providers: [
+    { provide: BASE_PATH, useValue: environment.API_BASE_PATH }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
